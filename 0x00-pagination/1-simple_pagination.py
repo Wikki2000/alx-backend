583eac4b9a224634ffaa2e5593de2c42415decec
wikki2000 <wisdomokposin@gmail.com>
2024-10-27 16:04:21 +0100

    Update:  Pycodestyle compliance

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -41,6 +41,14 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Paginate a large dataset from csv file.
+
+        :page - The number of page to paginate and it's 1-index.
+        :page_size - The number or size of each page.
+
+        :rtype - The paginated dataset.
+        """
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
 
