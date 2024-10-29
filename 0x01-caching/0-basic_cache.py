4ab375e74e52ae803f7676fbf972afb4912a1f85
wikki2000 <wisdomokposin@gmail.com>
2024-10-29 15:16:02 +0100

    Update the repo

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""BasicCache module
+"""
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+
+    def put(self, key, item):
+        """ 
+        Put an item in a dict with corresponding key.
+
+        :key - The key to store the item in dict.
+        :item - the item to be store in dict.
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieve an item from the dict with it right key.
+
+        :key - The key of item to be retrieved.
+        """
+        data = self.cache_data.get(key)
+        if not data:
+            return None
+        return data
+
+
